25b8c59914f06c3dfdcb28de7205637a25ebb624
Geo3ngel <GeoEngel.z@gmail.com>
2016-02-20 20:13:06 -0800

    Made some adjustments to The MapGrid System for size scaling

--- src/Mapping/MapsGrid.java
+++ src/Mapping/MapsGrid.java
@@ -4,10 +4,13 @@ import java.awt.Point;
 
 public class MapsGrid {
 
+	//Map, Tile, and Sprite Scaling number
+	int scale = 1;
+	
 	//Maps should be tiles in Lists with points
 	//Aligns Things along a 32 point grid
 	public Point gridCords(int x, int y){
-		Point p = new Point(x * 32,y * 32);
+		Point p = new Point(x * 32 * scale,y * 32 * scale);
 		return p;
 	}
 	
